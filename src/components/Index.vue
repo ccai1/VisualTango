a3b43e9359f053da113eb37b336345c4dc895fc4
Taikun Guo <billg1990@gmail.com>
2018-02-24 18:55:13 -0500

    finally got blender model displayed in three.js.

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -17,23 +17,41 @@ export default {
   },
   mounted () {
     const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-    const renderer = new THREE.WebGLRenderer()
-    renderer.setSize(window.innerWidth, window.innerHeight)
+
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
+    camera.position.set(0, 10, 15)
+    camera.lookAt(scene.position)
+
+    const renderer = new THREE.WebGLRenderer({ antialias : true })
+    renderer.setSize(window.innerWidth * 0.9, window.innerHeight * 0.9)
     document.body.appendChild(renderer.domElement)
 
-    let geometry = new THREE.BoxGeometry(1, 1, 1)
-    let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-    let cube = new THREE.Mesh(geometry, material)
-    console.log(scene)
-    scene.add(cube)
+    const loader = new THREE.JSONLoader()
+    loader.load(
+      // file path
+      'static/model/stickmale.json', 
+      
+      // onLoad
+      (geometry) => {
+        let mesh = new THREE.Mesh(geometry)
+        scene.add(mesh)
+      },
 
-    camera.position.z = 5
+      // onProcess
+      (xhr) => {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded')
+      },
+
+      // onError
+      (err) => {
+        console.log('[ERROR] when loading model file: ', err)
+      }
+    )
 
     let animate = function () {
       requestAnimationFrame(animate)
-      cube.rotation.x += 0.1
-      cube.rotation.y += 0.1
+      // cube.rotation.x += 0.1
+      // cube.rotation.y += 0.1
       renderer.render(scene, camera)
     }
 
