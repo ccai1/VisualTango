b2ccd9da23843df9fe3adba967e68e35a0d79f1e
Taikun Guo <billg1990@gmail.com>
2018-04-28 19:03:29 -0400

    add three.js scene to the right panel.

--- src/components/MainFrame.vue
+++ src/components/MainFrame.vue
@@ -3,6 +3,70 @@
   </div>
 </template>
 
+<script>
+/* eslint-disable */
+// load THREE.js
+import * as THREE from 'three'
+
+export default {
+  data () {
+    return {}
+  },
+  mounted () {
+    const scene = new THREE.Scene()
+    // add axis for debugging
+    // x is red, y is green, z is blue
+    const axesHelper = new THREE.AxesHelper(10)
+    scene.add(axesHelper)
+
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
+    camera.position.set(10, 10, 10)
+    camera.lookAt(scene.position) // look at 0, 0, 0
+
+    const renderer = new THREE.WebGLRenderer({ antialias : true })
+    renderer.setSize(this.$el.clientWidth, this.$el.clientHeight)
+    this.$el.appendChild(renderer.domElement)
+
+    let mixer, set
+    let clock = new THREE.Clock()
+
+    const loader = new THREE.JSONLoader()
+    loader.load('static/model/stickman_animated.json', (geometry) => {
+      let male = new THREE.SkinnedMesh(geometry, 
+        new THREE.MeshBasicMaterial({ 
+          color: 0x0080ff,
+          skinning: true
+        })
+      )
+      // let female = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 0xff0080 }))
+      scene.add(male)
+      // scene.add(female)
+      // console.log(male.position)
+      // male.position.set(3, 0, -6)
+      // female.position.set(-3, 0, -6)
+
+      mixer = new THREE.AnimationMixer(male)
+      let clips = geometry.animations
+      let walkingClip = clips[0]
+      let walkCycle = mixer.clipAction(walkingClip)
+      walkCycle.play()
+
+      set = true
+    })
+
+    let animate = function () {
+      if (set) {
+        mixer.update(clock.getDelta())
+      }
+      requestAnimationFrame(animate)
+      renderer.render(scene, camera)
+    }
+
+    animate()
+  }
+}
+</script>
+
 <style scoped>
 .main-frame {
   position: relative;
@@ -13,5 +77,10 @@
   height: 98%;
   background-color: #000000;
   float: right;
+}
+canvas {
+  position: relative;
+  width: 100%;
+  height: 100%
 }
 </style>
